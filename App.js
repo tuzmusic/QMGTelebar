51cddbe1ebe9482e50c7f51da5f502ee713fd90e
tuzmusic <tuzmusic@gmail.com>
2019-07-24 17:58:04 -0400

    remove StyleSheet call

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -9,11 +9,11 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+};
