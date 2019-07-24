331d8d033f52dd4b53d57389220f65ef56e56e7d
tuzmusic <tuzmusic@gmail.com>
2019-07-24 18:02:59 -0400

    add setup functions and wrap app in provider

--- App.js
+++ App.js
@@ -7,11 +7,15 @@ import type { Saga } from "redux-saga";
 import productsReducer from "./src/redux/reducers/productsReducer";
 import productSaga from "./src/redux/actions/productActions";
 
+const store = setupAndReturnStore();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </Provider>
   );
 }
 
@@ -23,3 +27,22 @@ const styles = {
     justifyContent: "center"
   }
 };
+
+function setupAndReturnStore() {
+  const combinedReducer = combineReducers({
+    productsReducer
+  });
+
+  function* rootSaga(): Saga<void> {
+    sagaMiddleware.run(productSaga);
+  }
+
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(
+    combinedReducer,
+    {},
+    applyMiddleware(sagaMiddleware)
+  );
+  sagaMiddleware.run(rootSaga);
+  return store;
+}
